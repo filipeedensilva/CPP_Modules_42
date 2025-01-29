28bf7603a12855f755d44bb9a95c4952689a49b4
Filipe Eden Peixote E Silva <feden-pe@c2r3s9.42lisboa.com>
2025-01-29 17:39:54 +0000

    Editing Array.hpp

--- cpp_07/ex02/includes/Array.hpp
+++ cpp_07/ex02/includes/Array.hpp
@@ -15,12 +15,36 @@ class Array
 
 	public:
 		Array() : _size(0), _values(NULL) {}
-		Array(unsigned int n) : _size(n), _values(new T[n]) {}
-		Array(const Array &copy);
-		Array &operator=(const Array &copy);
-		~Array();
+		Array(size_t n) : _size(n), _values(new T[n]) {}
+		Array(const Array &copy) {
+			_values = new T[copy.getSize()];
+			_size = copy.getSize;
+			for (size_t i = 0; i < copy.getSize(); i++)
+				_values[i] = copy._values[i];
+		}
+		Array &operator=(const Array &copy) {
+			if (this != copy) {
+				if (_values)
+					delete[] _values;
+				_size = copy.getSize();
+				_values = new T[copy.getSize()];
+				for (size_t i = 0; i < copy.getSize(); i++)
+					_values[i] = copy._values[i];
+			}
+			return (*this);
+		}
+		~Array() {
+			delete[] _values;
+		}
 
-		int	size(void);
+		size_t	getSize(void) const { return (this->_size); };
+
+		class	OutOfBoundsExceptions : public std::exception {
+			public :
+				const char* what() const throw() {
+					return ("Error: index out of bounds");
+				}
+		};
 };
 
 #endif
